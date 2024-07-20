ba455d354366c6d6b6d54ec96b7ce8b0cf49c420
Ksenija <omega-2077@list.ru>
2024-07-20 17:41:52 +0300

    change src3

--- src/test/java/ru/netology/stats/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -18,4 +18,28 @@ public class StatisticsServiceTest {
 
 
     }
+
+    @Test
+    void findMaxim() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 15, 8, 4, 25, 3, 8, 6, 11, 11, 12};
+        long expected = 25;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaximum() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
